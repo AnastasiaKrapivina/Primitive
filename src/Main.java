0e216dc913c3b65a6da505b1cac87c443539dd5a
Anastasia Krapivina <krapivina934@gmail.com>
2023-03-21 15:47:41 +0700

    Cheange one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ticket = 5725; // стоимость билета
+        int mile = 20; // количество рублей для одной бонусной мили
+        int bonus = ticket / mile; //количество бонусных миль
+
+        System.out.println(bonus + " Количество бонусных миль");
     }
 }
